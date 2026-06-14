dc519c9947f92c439e97350851d8b6a0bdf827e3
Tianxiang Wang <270430358@yoobeestudent.ac.nz>
2026-06-14 12:31:25 +1200

    1

--- week10/activity1/regression.py
+++ week10/activity1/regression.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+import numpy as np
 
 ## This dataset lacks senior level experience data, so the model may overestimate salaries for higher experience levels.
 
@@ -31,5 +32,19 @@ sns.regplot(
 axes[1].set_title("Polynomial Regression Degree 3")
 
 plt.tight_layout()
-plt.tight_layout()
 plt.savefig("week10/activity1/regression_comparison.png", dpi=300)
+
+## Polynomial regression is better than linear regression.
+linear_model = np.poly1d(np.polyfit(df["YearsExperience"], df["Salary"], 1))
+polynomial_model = np.poly1d(np.polyfit(df["YearsExperience"], df["Salary"], 3))
+
+for year in [14, 14.5, 15]:
+    print(f"{year} years linear predicted salary: {linear_model(year):.2f}")
+    print(f"{year} years polynomial predicted salary: {polynomial_model(year):.2f}")
+
+""" 14 years linear predicted salary: 153441.74
+14 years polynomial predicted salary: 138926.10
+14.5 years linear predicted salary: 157973.48
+14.5 years polynomial predicted salary: 138763.90
+15 years linear predicted salary: 162505.22
+15 years polynomial predicted salary: 137905.07 """
